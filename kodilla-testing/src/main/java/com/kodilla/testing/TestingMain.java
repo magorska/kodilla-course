7442ea7810be198dd1cde8097c25068438eab38f
Marta Górska <magorska.95@gmail.com>
2021-04-26 10:41:06 +0200

    6.2 - pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        // Test Calculator
+        System.out.println("\n Test drugi:");
+        Calculator calculator = new Calculator();
+
+        double a = 7;
+        double b = 2;
+
+        double addAToB = calculator.getAddAToB();
+        if (addAToB == 9) {
+            System.out.println("Wynik prawidłowy!");
+        } else {
+            System.out.println("Wynik błędny!");
+        }
+
+        double subtractAToB = calculator.getSubtractAToB();
+        if (subtractAToB == 5) {
+            System.out.println("Wynik prawidłowy!");
+        } else {
+            System.out.println("Wynik błędny!");
+        }
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public double addAToB(double a, double b) {
+        return a + b;
+    }
+
+    public double getAddAToB() {
+        return addAToB;
+    }
+    public double subtractAToB(double a, double b) {
+        return b - a;
+    }
+
+    public double getSubtractAToB() {
+        return subtractAToB;
+    }
+}
