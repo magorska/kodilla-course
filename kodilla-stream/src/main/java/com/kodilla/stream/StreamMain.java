c86c2c78e18743b5f7a35aca4a38cb5cd1f47b0f
Marta Górska <magorska.95@gmail.com>
2021-05-10 10:41:33 +0200

    7.1 PoemBeautifier

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
 public class StreamMain {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+
+//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+//
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+//
+//        System.out.println("Calculating expressions with method references");
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        String beautifulText = poemBeautifier.beautify("Text to beautify", text -> "***" + text + "***");
+        System.out.println(beautifulText);
+
+        String beautifulText2 = poemBeautifier.beautify("Text to beautify", text -> text.toUpperCase());
+        System.out.println(beautifulText2);
+
+        String beautifulText3 = poemBeautifier.beautify("Text to beautify", text -> text.toLowerCase());
+        System.out.println(beautifulText3);
+
+        String beautifulText4 = poemBeautifier.beautify("Text to beautify",text -> text.replace(" ","&"));
+        System.out.println(beautifulText4);
+
+        String beautifulText5 = poemBeautifier.beautify("Text to beautify", text -> text.repeat(3));
+        System.out.println(beautifulText5);
+
+
     }
 }
+
