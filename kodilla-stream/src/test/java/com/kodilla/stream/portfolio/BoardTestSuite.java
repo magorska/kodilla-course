4786560cc653139e21b1405911ca338d9f24bced
Marta Górska <magorska.95@gmail.com>
2021-05-13 13:21:38 +0200

    7.6 BoardTestSuit

--- kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
@@ -1,9 +1,12 @@
 package com.kodilla.stream.portfolio;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.*;
@@ -138,10 +141,22 @@ class BoardTestSuite {
         //Then
         assertEquals(2, longTasks);
     }
-//    @Test
-//    void testAddTaskListAverageWorkingOnTask() {
-//
-//    }
 
+    @Test
+    void testAddTaskListAverageWorkingOnTask() {
+        //Given
+        Board project = prepareTestData();
+        // When
+        List<TaskList> inProgressTasks = new ArrayList<>();
+        inProgressTasks.add(new TaskList("In progress"));
+        double average = project.getTaskLists().stream()
+                .filter(inProgressTasks::contains)
+                .flatMap(tl -> tl.getTasks().stream())
+                .mapToDouble(t -> Period.between(t.getCreated(), LocalDate.now()).getDays())
+                .average()
+                .getAsDouble();
 
+        //Then
+        Assertions.assertEquals(10.0, average,0);
+    }
 }
