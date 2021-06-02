993144efe42775a3cf54832de1b599b1e8fa0198
Marta Górska <magorska.95@gmail.com>
2021-06-02 09:31:46 +0200

    13.2 allegro

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/myAllegro/MailService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/myAllegro/MailService.java
@@ -1,5 +1,9 @@
 package com.kodilla.good.patterns.myAllegro;
 
-public class MailService {
+public class MailService implements InformationService{
 
+    @Override
+    public void inform(User user) {
+        System.out.println("Thank you " + user + " for placing the order");
+    }
 }
