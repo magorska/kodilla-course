4d4a510ed9cbd3312bfa2b6d57a4fab7065820ee
Marta Górska <magorska.95@gmail.com>
2021-06-28 10:49:15 +0200

    23.4 Observer

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/KodillaHomeworkTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/KodillaHomeworkTestSuite.java
@@ -1,5 +1,7 @@
 package com.kodilla.patterns2.observer.homework;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 public class KodillaHomeworkTestSuite {
@@ -8,12 +10,46 @@ public class KodillaHomeworkTestSuite {
     void testUpdate() {
 
         //Given
+        KodillaHomework task1 = new KodillaHomework("23.1 Facade");
+        KodillaHomework task2 = new KodillaHomework("23.2 Decotator");
+        KodillaHomework task3 = new KodillaHomework("23.3 Aspect");
+        KodillaHomework task4 = new KodillaHomework("23.4 Observer");
+        KodillaHomework task5 = new KodillaHomework("23.5 Adapter");
 
-//        Student emli = new Student("Emma Lim");
-//        Student same = new Student("Sarah Meyer");
-//        Student jakr = new Student("Jay Kristoff");
-//        Student rima = new Student("Rick Martin");
-//        Student dama = new Student("Daniel MacHale");
+        task1.addTaskToCheck("");
+        task2.addTaskToCheck("");
+        task3.addTaskToCheck("");
+        task4.addTaskToCheck("");
+        task5.addTaskToCheck("");
+
+        Student emli = new Student("Emma Lim");
+        Student same = new Student("Sarah Meyer");
+        Student jakr = new Student("Jay Kristoff");
+
+        Mentor rima = new Mentor("Rick Martin");
+        Mentor dama = new Mentor("Daniel MacHale");
+
+        task1.registerObserver(rima);
+        task2.registerObserver(dama);
+        task3.registerObserver(rima);
+        task4.registerObserver(rima);
+        task5.registerObserver(dama);
+
+        //When
+
+        emli.update(task1);
+        same.update(task2);
+        jakr.update(task3);
+        emli.update(task4);
+        same.update(task5);
+
+        //Then
+        Assertions.assertEquals(2, dama.getUpdateCount());
+        Assertions.assertEquals(3, rima.getUpdateCount());
+
+
+
+
 
 
 
